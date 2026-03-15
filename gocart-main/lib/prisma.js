4b03256d4691df41cac377846ceb96c2f24dffb5
VaishnaviGC <vaishnavichandra2005@gmail.com>
2026-03-15 09:05:31 -0700

    updated prisma.js

--- gocart-main/lib/prisma.js
+++ gocart-main/lib/prisma.js
@@ -1,16 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { neonConfig, Pool } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import ws from "ws";
-
-neonConfig.webSocketConstructor = ws;
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaNeon(pool);
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
