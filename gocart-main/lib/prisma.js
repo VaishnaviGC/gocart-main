f5f90fecd3bc94743ab9c7b4369d0ba912442a60
VaishnaviGC <vaishnavichandra2005@gmail.com>
2026-03-15 08:50:28 -0700

    changes according to deployment

--- gocart-main/lib/prisma.js
+++ gocart-main/lib/prisma.js
@@ -1,19 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
 
-import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig } from '@neondatabase/serverless'
-import ws from 'ws'
+neonConfig.webSocketConstructor = ws;
 
-neonConfig.webSocketConstructor = ws
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(pool);
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = globalThis;
 
-const adapter = new PrismaNeon({ connectionString })
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-const prisma = global.prisma || new PrismaClient({ adapter })
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-if (process.env.NODE_ENV === 'development') {
-  global.prisma = prisma
-}
-
-export default prisma
+export default prisma;
